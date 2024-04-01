c4386316cd4415510190a4909d6507c65ee63d13
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-04-01 10:31:43 +0600

    api and send form data through api created

---
+++ src/app/api/register/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+    console.log(name, email, password);
+    return NextResponse.json({ message: "User Registerd" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error occurred while registering user" },
+      { status: 500 }
+    );
+  }
+}
--- src/components/ResgisterForm.jsx
+++ src/components/ResgisterForm.jsx
@@ -1,22 +1,73 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./ResgisterForm.module.css";
 import Link from "next/link";
 const UserInfo = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  async function handleSubmitForm(e) {
+    e.preventDefault();
+    if (!name || !email || !password) {
+      setError("All fields are necessery");
+    }
+    try {
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+      } else {
+        console.log("User Registration failed ...");
+      }
+    } catch (error) {
+      console.log("Error during registartion : ", error);
+    }
+  }
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmitForm}>
+      <h2>Registrer</h2>
       <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <button>Register</button>
+      {error && <span>{error}</span>}
       <p>
         <Link href="/login">
           Already have an accout? <strong>Sign In</strong>
